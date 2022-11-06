f96498a000974ef053c25df2d119a515a2aa36fd
alex-sandler <troubadour1488@gmail.com>
2022-11-07 01:05:07 +0300

    Convert a string to an array

--- Find the smallest integer in the array.js
+++ Find the smallest integer in the array.js
@@ -1,7 +1,7 @@
 class SmallestIntegerFinder {
     findSmallestInt(mas) {
       let rsl = Number.MAX_SAFE_INTEGER;
-      for(let i = 0; i < mas.length; i++) {
+      for(let i = 0; i < mas.length; i++) {S
         if (mas[i] < rsl) {
           rsl = mas[i];
         }
