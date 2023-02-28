4bf939381d7cc82db0b616ec213497c91ed37a30
alex-sandler <troubadour1488@gmail.com>
2023-02-28 23:43:03 +0300

    Two to One

---
+++ Two to One.js
@@ -0,0 +1,16 @@
+function longest(s1, s2) {
+    var newStr = s1.concat(s2);
+    var longestCharArr = unique(newStr).split("").sort();
+    
+    return longestCharArr.join("");
+  }
+  
+  function unique(str) {
+    var result = "";
+    for(var i = 0; i < str.length; i++) {
+      if(result.indexOf(str[i]) == -1) {
+        result += str[i];
+      }
+    }
+    return result;
+  }
