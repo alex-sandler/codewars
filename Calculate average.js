f1f29f5028663a2cf4ff218f8ab4abcd6b532377
alex-sandler <troubadour1488@gmail.com>
2023-03-03 01:34:52 +0300

    Calculate average.js

--- Calculate average.js
+++ Calculate average.js
@@ -1,11 +1,14 @@
 function findAverage(array) {
+    // your code here
     let total = 0;
     let average = 0;
-    for (let i = 0; i < array.length; i++) {
-      total += array[i]
-    }
-    average = total / array.length
-    if (array === []) {
+    if (array.length === 0) {
       return 0;
-    } else return average;
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        total += array[i]
+      }
+      average = total / array.length
+      return average;
+    }
   }
