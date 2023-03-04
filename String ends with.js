ffee54abe2471d32da3411eac37085905468c778
alex-sandler <troubadour1488@gmail.com>
2023-03-04 21:39:50 +0300

    String ends with1

---
+++ String ends with.js
@@ -0,0 +1,5 @@
+function solution(str, ending){
+    return str.split('').filter((_, i) => i >= str.length - ending.length).join('') == ending ? true : false;
+  }
+  
+  console.log(solution("longword", "word"));
