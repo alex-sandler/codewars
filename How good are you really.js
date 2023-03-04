40d6bba2bc4b6cc76500d66628f8d7ec732b6ba8
alex-sandler <troubadour1488@gmail.com>
2023-03-04 21:17:37 +0300

    How good are you really3

--- How good are you really.js
+++ How good are you really.js
@@ -2,5 +2,3 @@ function betterThanAverage(classPoints, yourPoints) {
     let classAverage = (classPoints.reduce((sum,current)=>sum+current,0))/classPoints.length
     return yourPoints > classAverage
   }
-
-  
