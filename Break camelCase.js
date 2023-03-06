9f6d56113afcce368a52ab60d8a149af6a0a375b
alex-sandler <troubadour1488@gmail.com>
2023-03-07 00:23:25 +0300

    Break camelCase1

---
+++ Break camelCase.js
@@ -0,0 +1,12 @@
+function solution(string) {
+    let splitStr = string.split("");
+    let newStr = "";
+    splitStr.forEach(e =>{
+        if(e === e.toUpperCase()) newStr +=" "+e;
+        else newStr += e;
+    });
+    return newStr;
+  }
+  
+  console.log(solution('camelCasing'));//success = "camel Casing"
+  console.log(solution('camelCasingTest'));
