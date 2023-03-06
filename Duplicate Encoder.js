31ca3bd3a9844fe0161d6554e69625dc3b41bf3b
alex-sandler <troubadour1488@gmail.com>
2023-03-07 01:01:29 +0300

    Duplicate Encoder1

---
+++ Duplicate Encoder.js
@@ -0,0 +1,5 @@
+function duplicateEncode(word) {
+    return word.toLowerCase().split('').map(function (_char, i, arr) {
+      return (arr.filter((item) => item === _char).length - 1) ? ')' : '(';
+    }).join('');
+  }
