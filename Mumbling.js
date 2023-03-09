79afce209d7954f4ee048c6c8a5b0e5f468bbadb
alex-sandler <troubadour1488@gmail.com>
2023-03-09 21:23:02 +0300

    Mumbling1

---
+++ Mumbling.js
@@ -0,0 +1,14 @@
+function accum(s) {
+  
+    let sArray = s.split('')
+    result = []
+    let b = 0
+   for(let i = 0 ; i < sArray.length ; i++){
+     b = sArray[i].toUpperCase()+sArray[i].toLowerCase().repeat(i).trim()
+     result.push(b)
+     
+     
+   } 
+    return result.join('-')
+    // your code
+  }
